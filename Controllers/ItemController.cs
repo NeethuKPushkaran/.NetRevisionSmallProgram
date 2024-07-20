78f653c293e1d686f6e94921f7ef264ec3d09f61
NeethuKPushkaran <neethusathya07@gmail.com>
2024-07-20 12:17:05 +0530

    first-commit

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Small_Program.Models;
+
+namespace Small_Program.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private static readonly List<Item> _items = new List<Item>
+        {
+            new Item { Id = 1, Name = "Item1", Price = 10, quantity = 2},
+            new Item { Id = 2, Name = "Item2", Price = 12, quantity = 3},
+            new Item { Id = 3, Name = "Item3", Price = 9, quantity = 4}
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Item>> GetItems()
+        {
+            return _items;
+        }
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace Small_Program.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int quantity { get; set; }
+    }
+}
